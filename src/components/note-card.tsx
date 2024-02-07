ad248a848017b61b9a86d29675c4d59c1c9ea867
Matheus Mello <matheusgmello1@gmail.com>
2024-02-07 10:09:52 -0300

    feat: add props interface

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,16 +1,19 @@
-export function NoteCard() {
+interface NoteCardProps {
+  note: {
+    date: Date
+    content: string
+  }
+}
+
+export function NoteCard({ note }: NoteCardProps) {
   return (
-    <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
-      <span className="text-sm font-medium text-slate-300">há 2 dias</span>
+    <button className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
+      <span className="text-sm font-medium text-slate-300">
+        {note.date.toISOString()}
+      </span>
 
       <p className="text-sm leading-6 text-slate-400">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus commodi
-        dolor quaerat similique asperiores minus vitae magni nemo animi iusto
-        ducimus sapiente, error placeat ratione vel. Voluptatibus sequi quis
-        ratione. Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-        commodi dolor quaerat similique asperiores minus vitae magni nemo animi
-        iusto ducimus sapiente, error placeat ratione vel. Voluptatibus sequi
-        quis ratione.
+        {note.content}
       </p>
 
       <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
